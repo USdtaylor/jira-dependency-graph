69bdda47eeb8f958300908cb28bfc8a001d642d8
USdtaylor <dtaylor@unlimitedsystems.com>
2017-12-07 10:25:55 -0500

    everything working, waiting on Bill

--- api/index.js
+++ api/index.js
@@ -6,9 +6,11 @@ const JiraClient = require('./jira-client')
 app.use(cors())
 app.options('*', cors());
 
-app.get('/', async (request, response) => {
+app.get('/api/getIssues', async (req, res) => {
+   const epic = req.query.epic
+   if (!epic) { return res.status(400).end() }
    const client = await JiraClient('https://sharpts.atlassian.net', 'dtaylor@unlimitedsystems.com', '"skybomb0"')
-   const searchResults = await client.searchAll('"Epic Link" = NIM-25')
+   const searchResults = await client.searchAll(`"Epic Link" = ${epic}`)
    const issues = searchResults.map(issue => ({
       issueKey: issue.key,
       summary: issue.fields.summary,
@@ -21,7 +23,7 @@ app.get('/', async (request, response) => {
             : ({ type: link.type.inward, key: link.inwardIssue.key })
       )
    }))
-   response.json(issues)
+   res.json(issues)
 })
 
 const port = 3000
